0e92a017566f161684be4cb85d1e68eaa5d8492d
Quinn Haverstick <qh9929@princeton.edu>
2022-11-12 19:24:29 -0500

    added code to parse csv file

--- pySerial.py
+++ pySerial.py
@@ -1,25 +1,44 @@
 import serial
 import time
+import csv
 
 arduino = serial.Serial(port = "COM3", baudrate=115200, timeout=.1)
 
 wordsBad = ["shit", "asshole", "bitch", "poop", "piss"]
 wordsVeryBad = ["fuck", "dick", "cock", "pussy", "tits", "cunt"]
+wordsIn = []
+startsIn = []
+endsIn = []
+
+
 
 def write(x):
     arduino.write(bytes(x, 'utf-8'))
     time.sleep(0.01)
     return 0
 
-while True:
-    inputWord = input("Enter a word: ").lower()
+with open('test.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        wordsIn.append(row['Word'].lower())
+        startsIn.append(row['start_time'])
+        endsIn.append(row['end_time'])
+
+print("done")
+
+g = 0
+for h in wordsIn: 
+    hStart = int(startsIn[g])
+    hEnd = int(endsIn[g])
     for i in wordsBad:
-        if i in inputWord:
+        if i in h:
             write("1")
         else:
             write("0")
     for j in wordsVeryBad:
-        if j in inputWord:
+        if j in h:
             write("4")
         else:
             write("0")
+    time.sleep((hEnd - hStart) * 0.25)
+    g += 1
